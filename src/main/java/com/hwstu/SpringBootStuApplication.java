cc55aeb6b93fec2fda4ebbce592d622ad9757ebd
hjy628 <hjy628@gmail.com>
2021-01-23 20:56:09 +0800

    Initial commit

---
+++ src/main/java/com/hwstu/SpringBootStuApplication.java
@@ -0,0 +1,13 @@
+package com.hwstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hwstu/SpringBootStuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwstu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
