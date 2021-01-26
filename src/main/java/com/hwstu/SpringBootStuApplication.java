df819982acd1be4be489ae7194000a2bd0c67c6a
hjy628 <hjy628@gmail.com>
2021-01-26 16:23:35 +0800

    T

--- src/main/java/com/hwstu/SpringBootStuApplication.java
+++ src/main/java/com/hwstu/SpringBootStuApplication.java
@@ -1,13 +1,20 @@
 package com.hwstu;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+//@ServletComponentScan(basePackages = "com.hwstu.web.servlet")
 public class SpringBootStuApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootStuApplication.class, args);
+        new SpringApplicationBuilder(SpringBootStuApplication.class)
+//                .web(WebApplicationType.NONE)
+                .run(args);
+//        SpringApplication.run(SpringBootStuApplication.class, args);
     }
 
 }
